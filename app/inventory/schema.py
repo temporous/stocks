5ed56e9b3baa573900bccdcb8beea6d5585d5454
Gabriel Östlund <griffonious+git@gmail.com>
2022-11-13 20:56:42 +0100

    Add factory for meta models needed by graphene
    Graphene django requires a class Meta that specifies model and fields for a node.
    
    Since we plan to generate this part dynamically we need a factory to do that for us.
    
    Since the model is fairly simple and we don't really need to change anything,
    we can have a plain class factory and don't need to use type or derivatives.

--- app/inventory/schema.py
+++ app/inventory/schema.py
@@ -10,3 +10,22 @@ from graphene_django.filter import DjangoFilterConnectionField
 from inventory.models import Portfolio, Stock, Trade
 
 inventory_models = [Portfolio, Stock, Trade]
+
+
+def MetaFactory(a_model: Model, exclude: list[str] = []):
+    """
+    Class factory to create Meta models required for django graphene
+    """
+    fields = tuple(
+        f.name
+        for f in a_model._meta.get_fields()
+        if f.concrete and not f.name in exclude
+    )
+
+    class Meta:
+        model = a_model
+        fields = "__all__"
+        filter_fields = fields
+        interfaces = (relay.Node,)
+
+    return Meta
